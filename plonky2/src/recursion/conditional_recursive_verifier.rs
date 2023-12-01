9d982f788271f0700be3c234fb162b72a99063dc
kbizikav <132550763+kbizikav@users.noreply.github.com>
2023-12-01 13:30:35 +0100

    change visibility

--- plonky2/src/recursion/conditional_recursive_verifier.rs
+++ plonky2/src/recursion/conditional_recursive_verifier.rs
@@ -137,7 +137,7 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
     }
 
     /// Computes `if b { h0 } else { h1 }`.
-    pub(crate) fn select_hash(
+    pub fn select_hash(
         &mut self,
         b: BoolTarget,
         h0: HashOutTarget,
